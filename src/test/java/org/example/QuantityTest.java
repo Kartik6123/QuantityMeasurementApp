04928c5cf9be29cd5e14b4fa5ed4469f02242417
Your Name <youremail@example.com>
2026-03-17 14:54:25 +0530

    [Kartik Arora] Add . Solved use case 17

--- src/test/java/org/example/QuantityTest.java
+++ src/test/java/org/example/QuantityTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
-import UtilityClasses.*;
+
+import com.app.quantitymeasurement.unit.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
